a26ba08675576b78ad86e886f66b33ae6d3bb028
suhyun-h <suhyun8913@naver.com>
2025-08-20 17:41:19 +0900

    searchProduct(), registProduct() 추가

--- snack-management/src/main/java/com/sm/testapp/service/ProductService.java
+++ snack-management/src/main/java/com/sm/testapp/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.sm.testapp.service;
 
+import com.sm.testapp.aggregate.ProduceStatus;
 import com.sm.testapp.aggregate.Product;
 import com.sm.testapp.repository.ProductRepository;
 
@@ -22,6 +23,31 @@ public class ProductService {
         }
         else{
             System.out.println("존재하는 제품이 없습니다.");
+        }
+    }
+
+    public void searchProduct(String proName) {
+        Product searchPro = productRepository.searchProduct(proName);
+        if(searchPro != null){
+            System.out.println("제품 조회 성공: " + searchPro);
+        }
+        else{
+            System.out.println(proName + " 제품이 존재하지 않습니다.");
+        }
+    }
+
+    public void registProduct(Product registPro) {
+        int lastNum = productRepository.findLastProductNum();
+        registPro.setProNo(lastNum + 1);
+
+        registPro.setProduceStatus(ProduceStatus.INPRODUCTION);
+
+        int result = productRepository.registProduct(registPro);
+        if(result == 1){
+            System.out.println(registPro.getProNo() + "번 " + registPro.getProName() + "제품이 등록되었습니다.");
+        }
+        else{
+            System.out.println("제품 등록 실패");
         }
     }
 }
